bad0ce7f8add3572769f68cfef95f097add8f1eb
Jess <lovebones93@gmail.com>
2018-03-11 21:07:44 +1100

    Hardcoded frontend and some of backend

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -8,7 +9,6 @@
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
   </head>
   <body>
-
     <nav class="navbar is-transparent">
       <div class="navbar-brand">
         <a class="navbar-item" href="https://bulma.io">
@@ -84,74 +84,6 @@
       </div>
     </nav>
 
-
-
-
-
-
-    <section class="hero is-primary is-medium">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">
-            Jessica Warburton
-          </h1>
-          <h2 class="subtitle">
-          Melbourne Web Developer</br>    <i class="icon is-medium fab fa-github"></i>
-               <i class="icon is-medium fab fa-twitter"></i>
-          </h2>
-          <p>
-Aldus Corporation, which later merged with Adobe Systems, helped bring Lorem Ipsum into the information age with its 1985 flagship software Aldus PageMaker. The program came bundled with Lorem Ipsum dummy text to help with laying out page content, and other word processors like Microsoft Word followed suit. More recently the growth of web design has helped proliferate Lorem Ipsum across the internet as a placeholder for laying out websites and blogs– and in some cases the final content (don't let that be you!).
-          </p>
-        </div>
-      </div>
-    </section>
-
-  </section>
-
-  <section class="section">
-    <div class="container">
-      <div class="columns">
-        <div class="column">
-          <h3 class="title is-3"> Blog Posts</h3>
-          <a href="#">View All Blog Posts</a>
-          <hr>
-          <p><a>How to win the lottery and stop worrying about atomic chaos</a></p>
-          <p><a>How to win the lottery and stop worrying about atomic chaos</a></p>
-          <p><a>How to win the lottery and stop worrying about atomic chaos</a></p>
-          <p><a>How to win the lottery and stop worrying about atomic chaos</a></p>
-        </div>
-        <div class="column">
-          <h3 class="title">Recent Projects
-          </h3>
-          <a href="#">View All Projects</a>
-          <hr>
-          <p><a>How to win the lottery and stop worrying about atomic chaos</a></p>
-          <p><a>How to win the lottery and stop worrying about atomic chaos</a></p>
-          <p><a>How to win the lottery and stop worrying about atomic chaos</a></p>
-          <p><a>How to win the lottery and stop worrying about atomic chaos</a></p>
-        </div>
-
-      </div>
-    </div>
-
-  </section>
-
-
-
-
-
-  <footer class="footer">
-    <div class="container">
-      <div class="content has-text-centered">
-        <p>
-          <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-        </p>
-      </div>
-    </div>
-  </footer>
-
-
+ @yield('content')
   </body>
 </html>
