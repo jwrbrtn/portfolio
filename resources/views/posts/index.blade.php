2314dfff82b61361befeb75b53d7df104e72c13c
Jess <lovebones93@gmail.com>
2018-03-15 16:56:39 +1100

    Make cosmetic changes and edit navbars on fronend

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -19,12 +19,12 @@
   @foreach($posts as $post)
   <div class="columns">
   <div class="column is-one-quarter">
-    <img src="https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=468a8c18f5d811cf03c654b653b5089e&w=1000&q=80" alt="">
+    <img class="" src="/images/{{$post->featureimage}}" alt="">
   </div>
   <div class="column">
     <h3 class="title is-3"><a href="/blog/{{$post->id}}">{{$post->title}}</a></h3>
-    <h3 class="subtitle is-6">Published {{ Carbon\Carbon::parse($post->created_at)->diffForHumans() }} by Jessica Warburton</h3>
-    <p>{{$post->body}}</p></br>
+    <h3 class="subtitle is-6">Published {{ Carbon\Carbon::parse($post->created_at)->diffForHumans() }} by <a>Jessica Warburton</a></h3>
+    <p>{!! $post->body !!}</p></br>
   </div>
 
 
