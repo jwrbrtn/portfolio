048be3bbfb89705cddfee7031d3bc7f3d6fc94de
Jess <lovebones93@gmail.com>
2018-03-30 23:26:32 +1100

    Adds small typographic changes

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -24,7 +24,8 @@
   <div class="column">
     <h3 class="title is-3"><a href="/blog/{{$post->id}}">{{$post->title}}</a></h3>
     <h3 class="subtitle is-6">Published {{ Carbon\Carbon::parse($post->created_at)->diffForHumans() }} by <a>Jessica Warburton</a></h3>
-    <p>{!! $post->body !!}</p></br>
+      <p class="blurb">{!! $post->body !!}</p></br>
+    <a class="button is-primary" href="/blog/{{$post->id}}" type="button" name="button">Read More</a>
   </div>
 
 
