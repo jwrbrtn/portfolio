2cd9541ce60b4ef81596d0a477d325ab925058de
Jess <lovebones93@gmail.com>
2018-04-04 22:05:31 +1000

    Makes footer dynamic on homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,9 +102,7 @@
     <div class="container">
       <div class="content has-text-centered">
         <p>
-          <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+          {{$settings->footer}}
         </p>
       </div>
     </div>
