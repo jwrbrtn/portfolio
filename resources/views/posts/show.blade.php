d536f0ade1304b0b7ece6739dbaffc24e26fb259
Jess <lovebones93@gmail.com>
2018-04-06 00:38:12 +1000

    Updates css text styling

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,17 +3,18 @@
 <section class="hero is-primary">
   <div class="hero-body">
     <div class="container">
-      <h1 class="title">
-        {{$post->title}}
-      </h1>
-      <h1 class="subtitle has-text-weight-normal">Published: {{ Carbon\Carbon::parse($post->created_at)->format('D d M Y') }} By Jessica Warburton </h1>
-
+      <p><a href="/">Home</a> > <a href="/blog">Blog</a></p>
     </div>
   </div>
 </section>
 <div class="container content">
 <div class="columns">
 <div class="column is-three-fifths">
+  <h1 class="title">
+    {{$post->title}}
+  </h1>
+  <h3 class="is-size-6 byline">Published: {{ Carbon\Carbon::parse($post->created_at)->format('D d M Y') }} By Jessica Warburton </h3>
+
 <p class="has-text-justified">{!! $post->body !!}</p>
 </div>
 <div class="column is-one-half">
