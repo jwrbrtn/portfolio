24b5f40879dd26dd9fc437d9f53a6533d1bb6ea8
Jess <lovebones93@gmail.com>
2018-04-06 04:10:33 +1000

    Adds styling to message show view

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -3,9 +3,27 @@
 
 <div class="container form-section">
 
-<h1 class="is-title">From: {{$message->name}}</h1>
-<h1 class="is-title">Email: {{$message->email}}</h1>
-<p>{{$message->body}}</p>
+
+
+<div class="card">
+  <header class="card-header">
+    <p class="card-header-title">
+      From: {{$message->name}}
+
+    </p>
+
+  </header>
+  <div class="card-content">
+    <div class="content">
+      <time datetime="2016-1-1"><i>Recieved: {{ Carbon\Carbon::parse($message->created_at)->format('D d M Y') }}</i></time></br></br>
+      <p>{{$message->body}}</p>
+    </br></br>
+      <a href="#">{{$message->email}}</a>
+    </div>
+  </div>
+</div>
+
+
 </div>
 
 @endsection
