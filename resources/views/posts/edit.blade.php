3e1c7ffbc511332a369fa7bb2e15154f2d371410
Jess <lovebones93@gmail.com>
2018-04-07 04:34:05 +1000

    Fixes edit views for projects and posts

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,21 +1,74 @@
-@extends('layouts.app')
+@extends('layouts.backend')
 @section('content')
-<div class="container">
-<h1>Edit Post</h1>
-<form class="" action="/blog/{{$post->slug}}/edit" method="post">
-  {{csrf_field()}}
-  <input type="text" class="form-control" name="title" value="{{$post->title}}"></br>
-  <textarea class="form-control" name="body" rows="8" cols="80">{{$post->body}}</textarea></br>
-        <input type="hidden" name="_method" value="PATCH">
-    <input class="btn btn-primary" type="submit" name="Submit" value="Submit">
-</form>
+<div class="container form-section">
+  @if ($errors->any())
+  <div class="notification is-danger">
+    <button class="delete"></button>
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+  </div>
+  @endif
 
-<form class="" action="/blog/{{$post->slug}}/delete" method="post">
-  {{csrf_field()}}
-        <input type="hidden" name="_method" value="DELETE">
-    <input class="btn btn-danger" type="submit" name="Submit" value="Delete">
-</form>
+  <h1 class="title">Edit post</h1>
+<form class="" action="/blog/{{$post->slug}}/edit" method="post" enctype="multipart/form-data">
+<div class="field">
+  <label class="label">Name</label>
+  <div class="control">
+    <input name="title" class="input" type="text" value="{{$post->title}}" placeholder="Text input">
+    {{csrf_field()}}
+  </div>
+</div>
 
+<input type="hidden" name="_method" value="PATCH">
+
+<div class="field">
+  <label class="label">Slug</label>
+  <div class="control">
+    <input name="slug" class="input" value="{{$post->slug}}" type="text" placeholder="Text input">
+
+  </div>
+</div>
+
+
+  <label class="label">Set feature image</label>
+  <div class="file">
+    <label class="file-label">
+      <input class="file-input" type="file" name="featureimage">
+      <span class="file-cta">
+        <span class="file-icon">
+          <i class="fas fa-upload"></i>
+        </span>
+        <span class="file-label">
+          Choose a file…
+        </span>
+      </span>
+    </label>
+  </div>
+
+<div class="field">
+  <label class="label">Message</label>
+  <div class="control">
+  <textarea id="summernote" name="editordata">{{$post->body}}</textarea>
+  </div>
+</div>
+
+
+
+
+
+<div class="field is-grouped">
+  <div class="control">
+    <input type="submit" class="button" name="" value="Submit">
+  </div>
+  <div class="control">
+    <button class="button is-text">Cancel</button>
+  </div>
+</div>
+
+</form>
 </div>
 
 
--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -1,21 +1,74 @@
-@extends('layouts.app')
+@extends('layouts.backend')
 @section('content')
-<div class="container">
-<h1>Edit Project</h1>
+<div class="container form-section">
+  @if ($errors->any())
+  <div class="notification is-danger">
+    <button class="delete"></button>
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+  </div>
+  @endif
+
+  <h1 class="title">Edit Project</h1>
 <form class="" action="/projects/{{$project->slug}}/edit" method="post">
-  {{csrf_field()}}
-  <input type="text" class="form-control" name="title" value="{{$project->title}}"></br>
-  <textarea class="form-control" name="body" rows="8" cols="80">{{$project->body}}</textarea></br>
-        <input type="hidden" name="_method" value="PATCH">
-    <input class="btn btn-primary" type="submit" name="Submit" value="Submit">
-</form>
+<div class="field">
+  <label class="label">Name</label>
+  <div class="control">
+    <input name="title" class="input" type="text" value="{{$project->title}}" placeholder="Text input">
+    {{csrf_field()}}
+  </div>
+</div>
 
-<form class="" action="/projects/{{$project->slug}}/delete" method="post">
-  {{csrf_field()}}
-        <input type="hidden" name="_method" value="DELETE">
-    <input class="btn btn-danger" type="submit" name="Submit" value="Delete">
-</form>
+<input type="hidden" name="_method" value="PATCH">
 
+<div class="field">
+  <label class="label">Slug</label>
+  <div class="control">
+    <input name="slug" class="input" value="{{$project->slug}}" type="text" placeholder="Text input">
+
+  </div>
+</div>
+
+
+  <label class="label">Set feature image</label>
+  <div class="file">
+    <label class="file-label">
+      <input class="file-input" type="file" name="featureimage">
+      <span class="file-cta">
+        <span class="file-icon">
+          <i class="fas fa-upload"></i>
+        </span>
+        <span class="file-label">
+          Choose a file…
+        </span>
+      </span>
+    </label>
+  </div>
+
+<div class="field">
+  <label class="label">Message</label>
+  <div class="control">
+  <textarea id="summernote" name="editordata">{{$project->body}}</textarea>
+  </div>
+</div>
+
+
+
+
+
+<div class="field is-grouped">
+  <div class="control">
+    <input type="submit" class="button" name="" value="Submit">
+  </div>
+  <div class="control">
+    <button class="button is-text">Cancel</button>
+  </div>
+</div>
+
+</form>
 </div>
 
 
