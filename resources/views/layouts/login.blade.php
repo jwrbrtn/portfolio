834b104e06141f636548dd0c6f02d923caf2cbab
Jess <lovebones93@gmail.com>
2018-04-07 08:17:46 +1000

    Customize login view

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Hello Bulma!</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">
+<link rel="stylesheet" href="/css/login.css">
+<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+    <title>Login</title>
+  </head>
+  <body>
+
+@yield('content')
+
+  </body>
+</html>
